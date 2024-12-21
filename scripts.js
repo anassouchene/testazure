d1edad0157b25d06535d208e874a6ec740bb97d2
anassouchene <anassouchene27@gmail.com>
2024-12-21 12:29:09 +0100

    added files

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Exemple de script : afficher une alerte lors de la navigation
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("JavaScript chargé avec succès !");
+  });
+  
